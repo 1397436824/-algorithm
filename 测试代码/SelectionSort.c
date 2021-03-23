96fa157e86fb3f0fc5436597b6a1722e75dfbf6f
ouyangkang <1397436824@qq.com>
2021-03-23 11:30:32 +0800

    选择排序

---
+++ 测试代码/SelectionSort.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "selection.h"
+
+
+
+
+
+
+
+//选择排序
+//seq:需要排列的数组
+//length:需要排序数组的长度(元素个数)
+//成功：返回0
+//失败：返回-1
+int SelectionSort(int *seq,int length)
+{
+	int p0 = 0,i;
+	int len = length -1;
+	if(seq == NULL) return -1;
+	for(i)
+	return 0;
+}
